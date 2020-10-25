08c2e767b52bf63106395fb37320cc1da7f1924a
hritu567 <hritumog96@gmail.com>
2020-10-25 23:13:51 +0530

    Added : for multiple companies

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -3,12 +3,9 @@ package com.bridgelabz;
 public class EmpWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_OF_WORKING_DAYS=2;
-	public static final int MAX_HOURS_IN_MONTH=10;
-	public static int computeWage() {
+	public static int computeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		int empHrs = 0, totalEmpHrs=0, totalWorkingDays=0;
-		while (totalEmpHrs<=MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+		while (totalEmpHrs<=maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
 		{
 			 totalWorkingDays++;
 			 int empCheck=(int) Math.floor(Math.random() * 10) %3;
@@ -28,11 +25,12 @@ public class EmpWageBuilder {
 			 System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs);
     
 		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
 		System.out.println("Total Emp Wage : " + totalEmpWage);
 		return totalEmpWage;
 	}
 	public static void main(String[] args) {
-	computeWage();
-		}
+	computeWage("Google",20,4,10);
+
+	computeWage("Tesla",10,8,30);}
 }
