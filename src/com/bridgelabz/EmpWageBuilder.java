ac7f35663ea67fb2415d6caf4c9ae65dafac622c
hritu567 <hritumog96@gmail.com>
2020-10-26 23:57:39 +0530

    Added : emp wage and tot comapny wage

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -3,7 +3,19 @@ package com.bridgelabz;
 public class EmpWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
-	public static int computeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+	public EmpWageBuilder(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
+	{
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxHoursPerMonth=maxHoursPerMonth;
+	}
+	public void computeWage() {
 		int empHrs = 0, totalEmpHrs=0, totalWorkingDays=0;
 		while (totalEmpHrs<=maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
 		{
@@ -25,12 +37,19 @@ public class EmpWageBuilder {
 			 System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs);
     
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage : " + totalEmpWage);
-		return totalEmpWage;
+	  totalEmpWage = totalEmpHrs * empRatePerHour;
+	}
+	public String tosString()
+	{
+		return "Total emp wage for company " + company + " is :" + totalEmpWage;
 	}
 	public static void main(String[] args) {
-	computeWage("Google",20,4,10);
+	EmpWageBuilder google=new EmpWageBuilder("Google",20,4,10);
 
-	computeWage("Tesla",10,8,30);}
+	EmpWageBuilder Tesla=new EmpWageBuilder("Tesla",10,8,30);
+	google.computeWage();
+	System.out.println(google);
+	Tesla.computeWage();
+	System.out.println(Tesla);
+	}
 }
